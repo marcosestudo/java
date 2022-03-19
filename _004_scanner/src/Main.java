c54fb665b418453ea27e4da6b438bbd464bc7347
Marcos Fernando <estudogit@outlook.com>
2022-03-19 19:10:40 -0300

    scanner

--- _004_scanner/src/Main.java
+++ _004_scanner/src/Main.java
@@ -1,9 +1,52 @@
+import java.util.Locale;
+import java.util.Scanner;
 
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+//		// input de uma palavra		
+//		String x = sc.next();	
+//		// digitando sysout e tclando ctrl + espaço, o eclipse completa com println
+//		System.out.println("Você digitou: " + x);
+//		
+//		
+//		int x2 = sc. nextInt();
+//		System.out.println("Você digitou: " + x2);
+//		
+//		
+//		// pega a localidade do sistema, devemos mudar com o locale para considerarmos o ponto como 
+//		// separador de decimais
+//		double x3 = sc. nextDouble();
+//		System.out.println("Você digitou: " + x3);
+//		
+//
+//		// input de um caracter		
+//		char x4 = sc.next().charAt(0);	
+//		System.out.println("Você digitou: " + x4);
+						
+		
+//		// input de um caracter		
+//		String x5 = sc.nextLine();	
+//		System.out.println("Você digitou: " + x5);
+		
+		
+		// quebra de linha pendente		
+		// quando damos enter pro nextInt(), a quebra de linha do enter fica pendente na entrada padrão 
+		// o próximo nextLine() consom eessa quebra de linha
+		// a solução é colocar um nextline() depois de um nextInt() ou next() 
+		int x2 = sc. nextInt();
+		sc.nextLine();
+		String x5 = sc.nextLine();
+		System.out.println("Você digitou: " + x2);
+		System.out.println("Você digitou: " + x5);
+		
+		
+		sc.close();
+		
 	}
 
 }
