f81e804b3ce1aa3de154731e7187a3d227655b19
Marcos Fernando <estudogit@outlook.com>
2022-03-19 23:59:08 -0300

    _005_math

---
+++ _005_math/src/Exercicio.java
@@ -0,0 +1,37 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Exercicio {
+
+	public static void main(String[] args) {
+		
+//		int A, B, soma;
+//
+//		Scanner sc = new Scanner(System.in);
+//		
+//		A = sc.nextInt();
+//		B = sc.nextInt();
+//		soma = A + B;
+//		
+//		System.out.printf("Soma = " + soma);
+
+//		sc.close();
+		
+		
+//		Double raio, pi, area;
+//		pi = 3.14159;
+//		
+//	    Locale.setDefault(Locale.US);
+//		Scanner sc = new Scanner(System.in);
+//		
+//		raio = sc.nextDouble();
+//		
+//		area = pi * Math.pow(raio, 2);
+//		
+//		System.out.printf("A=%.4f%n", area);
+//		
+//		sc.close();
+				
+	}
+
+}
