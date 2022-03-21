0df65488b61eb9390e3ff640f4f3e4adf5007228
Marcos Fernando <estudogit@outlook.com>
2022-03-21 17:15:05 -0300

    switch case

--- _006_if_else/src/Main.java
+++ _006_if_else/src/Main.java
@@ -1,8 +1,28 @@
+import java.util.Locale;
+import java.util.Scanner;
 
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
+		int A;
+		
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Digite um número: ");
+		A = sc.nextInt();
+		
+		// Se tiver só uma comando dentro do if / else, as chaves {} são opcionais
+		if (A == 1) {
+			System.out.println("Você digitou 1");
+		} else if (A == 2) {
+			System.out.println("Você digitou 2");
+		} else {
+			System.out.println("Você não digitou nem um, nem 2");
+		}
+
+		sc.close();
 
 	}
 
