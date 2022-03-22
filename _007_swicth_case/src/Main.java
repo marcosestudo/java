069aa003884be572334f81bb32da6f0bb5e74417
Marcos Fernando <estudogit@outlook.com>
2022-03-22 17:06:22 -0300

    do while

--- _007_swicth_case/src/Main.java
+++ _007_swicth_case/src/Main.java
@@ -34,7 +34,9 @@
 import java.util.Scanner;
 
 public class Main {
+	
 	public static void main(String[] args) {
+		
 		Scanner sc = new Scanner(System.in);
 		int x = sc.nextInt();
 		String dia;
--- _008_condicao_ternaria/src/Main.java
+++ _008_condicao_ternaria/src/Main.java
@@ -2,6 +2,7 @@
 public class Main {
 
 	public static void main(String[] args) {
+		
 //		double preco = 34.5;
 //		double desconto;
 //		
