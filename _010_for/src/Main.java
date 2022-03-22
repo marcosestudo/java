8b1cd53f181f3bfc500407ea08a6426b637a4cae
Marcos Fernando <estudogit@outlook.com>
2022-03-22 16:39:07 -0300

    for

---
+++ _010_for/src/Main.java
@@ -0,0 +1,17 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+
+		for (int i=0; i<5; i++) {
+			System.out.println(i);
+		}
+
+		sc.close();
+
+	}
+
+}
