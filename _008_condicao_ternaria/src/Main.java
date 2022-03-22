1bf3104476997567f2a65307e09f155d30cbdadd
Marcos Fernando <estudogit@outlook.com>
2022-03-22 17:54:37 -0300

    string

--- _008_condicao_ternaria/src/Main.java
+++ _008_condicao_ternaria/src/Main.java
@@ -3,17 +3,19 @@ public class Main {
 
 	public static void main(String[] args) {
 		
-//		double preco = 34.5;
-//		double desconto;
-//		
-//		if (preco < 20.0) {
-//			desconto = preco * 0.1;
-//		} else {
-//			desconto = preco * 0.05;
-//		}
-
+		/*
 		double preco = 34.5;
-		//                (expressão) ? verdadeiro : falso
+		double desconto;
+		
+		if (preco < 20.0) {
+			desconto = preco * 0.1;
+		} else {
+			desconto = preco * 0.05;
+		}
+		*/
+		
+		double preco = 34.5;
+					   // (expressão) ? verdadeiro : falso
 		double desconto = (preco < 20.0) ? preco * 0.1 : preco * 0.05;
 		
 		System.out.println(desconto);
