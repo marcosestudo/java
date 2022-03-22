6e0d5085c9fb02d1e6ea2b33cf053524135f4b80
Marcos Fernando <estudogit@outlook.com>
2022-03-22 17:30:00 -0300

    bitwise

---
+++ _012__operadores_bitwise/src/Main.java
@@ -0,0 +1,14 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+//		Operadores bitwise para operações bit a bit
+//		
+//		& e bit a bit
+//		| ou bit a bit
+//		^ xor (xclusive or) bit a bit
+
+	}
+
+}
