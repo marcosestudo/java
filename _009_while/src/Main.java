ebfc057590473689f756333c62ab240694ffb358
Marcos Fernando <estudogit@outlook.com>
2022-03-21 23:31:13 -0300

    while

---
+++ _009_while/src/Main.java
@@ -0,0 +1,26 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+
+		int soma = 0;
+		
+		System.out.println("Insira um número");
+		int num = sc.nextInt();;
+
+		while (num != 0) {
+			System.out.println("Insira um número");
+			soma += num;
+			num = sc.nextInt();
+		}
+
+		System.out.println(soma);
+
+		sc.close();
+
+	}
+
+}
